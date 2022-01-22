965da1aadc4c43155a31761915b73f0a6def51d1
ViniVMA <ctt.viniciusvilela@gmail.com>
2022-01-22 16:40:31 -0300

    build, feat(*, Header): add global style to the project, start the header component

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from "components/Header";
+import { AppWrapper } from "./styles/app.style";
+
+function App() {
+  return (
+    <AppWrapper className="App">
+      <Header />
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+      <h1>Hello world</h1>
+    </AppWrapper>
+  );
+}
+
+export default App;
---
+++ src/styles/app.style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.div`
+  background-color: #121212;
+  color: #2a2a2a;
+`;
